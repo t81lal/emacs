7ce93d6c23bba930517945532308c9cfefb15b76
Bilal Tariq <biblthebest@hotmail.com>
2020-07-07 19:28:57 +0100

    init.el: clean up init.el

--- init.el
+++ init.el
@@ -1,11 +1,21 @@
+;;; init.el --- 'bilb-macs' entry point
 
-;; Stop package-initialize being called after init.el
+;;; Commentary:
+;; This is the entry point of my personal Emacs configuration.
+;; In this file, we do some pre-configuration stuff such as including package.el and use-package for
+;; pulling other packages from the (m)elpa repositories later on.
+
+;;; Code:
+
+;; Stop package-initialize being called after this file is executed as we call it ourselves
+;; manually to perform the initialisation
 (setq package-enable-at-startup nil)
 
 ;; Use package.el to get use-package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;; For important compatibility libraries like cl-lib
+;;  this line is taken from the org-ref README file
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
@@ -32,4 +42,8 @@
 (load-file "~/.emacs.d/config/init-haskell.el")
 (load-file "~/.emacs.d/config/init-org.el")
 
+;; all of the customize.el configuration code will be redirected to custom.el so as to not
+;; clutter this file
 (setq custom-file "custom.el")
+
+;;; init.el ends here
