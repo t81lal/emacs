44b87ce03ec8f5e0f9e0cb47596f24d177b628fa
Bilal Tariq <biblthebest@hotmail.com>
2020-07-07 16:22:57 +0100

    init-org: fix wording

--- config/init-org.el
+++ config/init-org.el
@@ -3,8 +3,8 @@
 ;;; Commentary:
 ;; I have a few things in mind I want to use Emacs org-mode for:
 ;;  1. Taking personal notes and keeping them organised, like a diary
-;;  2. Making notes on academic literature, notes and articles and linking them together (thinking roam)
-;;  3. Keeping a schedule and a list of tasks that need to be done completed
+;;  2. Making notes on academic literature, books and articles and linking them together (thinking roam)
+;;  3. Keeping a schedule and a list of tasks that need to be completed
 ;;  4. Scheduling reoccurrences, e.g. Duolingo reminders, break timers, extra curriculars, etc.
 
 ;;; Code:
